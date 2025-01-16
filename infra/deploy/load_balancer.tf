b002a569e07e85b8176878d7a26fe55aa8f8926e
Stefano Llerandi <stefano.llerandi@studbocconi.it>
2025-01-15 20:37:47 -0600

    added lb target group
    :

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -37,5 +37,16 @@ resource "aws_lb" "api" {
     aws_subnet.public_b.id
   ]
   security_groups = [aws_security_group.lb.id]
+}
 
+resource "aws_lb_target_group" "api" {
+  name        = "${local.prefix}-api"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+  port        = 8000
+
+  health_check {
+    path = "/api/health-check/"
+  }
 }
